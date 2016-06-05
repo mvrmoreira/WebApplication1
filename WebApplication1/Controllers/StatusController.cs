29a8d32191bdc72fb71264e2ef09685d62689913
Matheus Moreira <mvrmoreira@gmail.com>
2016-06-05 17:43:52 -0300

    Cria controlador de status

---
+++ WebApplication1/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class StatusController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("status")]
+        public HttpResponseMessage GetStatus()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { 
+                now = DateTime.UtcNow
+            });
+        }
+    }
+}
