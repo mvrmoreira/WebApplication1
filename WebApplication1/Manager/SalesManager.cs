e9a8fe23fed619e7e7960b8329ceb8fb17b055f9
Matheus Moreira <mvrmoreira@gmail.com>
2016-06-06 11:25:19 -0300

    Add queue integration

--- WebApplication1/Manager/SalesManager.cs
+++ WebApplication1/Manager/SalesManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +14,35 @@ namespace WebApplication1.Manager
     {
         public void QueueSale(CreateSaleRequest request)
         {
+            #region Mapping
 
+            var queueItem = new NixQueueItem();
+
+            queueItem.RequestKey = Guid.NewGuid().ToString();
+            queueItem.CreateDate = DateTime.UtcNow;
+            queueItem.AffiliationData.Email = request.Email;
+            queueItem.AffiliationData.MerchantName = "Imasters";
+            queueItem.AmountInCents = Convert.ToInt32(request.Amount * 100);
+            queueItem.Card.CreditCardBrand = "Mastercard";
+            queueItem.Card.CreditCardNumber = request.CreditCard.Number;
+            queueItem.Card.ExpMonth = request.CreditCard.ExpirationMonth.ToString();
+            queueItem.Card.ExpYear = request.CreditCard.ExpirationYear.ToString();
+            queueItem.Card.HolderName = request.CreditCard.HolderName;
+            queueItem.Card.SecurityCode = request.CreditCard.Cvv;
+
+            string queueItemJson = JsonConvert.SerializeObject(queueItem);
+
+            #endregion
+
+            #region Send to queue
+
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.QueueName"));
+
+            Client.Send(new BrokeredMessage(queueItemJson));
+
+            #endregion
         }
     }
 }
---
+++ WebApplication1/Models/NixQueueItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class NixQueueItem
+    {
+        public DateTime CreateDate { get; set; }
+
+        public string RequestKey { get; set; }
+
+        public long AmountInCents { get; set; }
+
+        public AffiliationData AffiliationData { get; set; }
+
+        public Card Card { get; set; }
+
+        public NixQueueItem()
+        {
+            AffiliationData = new AffiliationData();
+            Card = new Card();
+        }
+    }
+
+    public class AffiliationData
+    {
+        public string Email { get; set; }
+
+        public string MerchantName { get; set; }
+    }
+
+    public class Card
+    {
+        public string CreditCardNumber { get; set; }
+
+        public string SecurityCode { get; set; }
+
+        public string HolderName { get; set; }
+
+        public string ExpMonth { get; set; }
+
+        public string ExpYear { get; set; }
+
+        public string CreditCardBrand { get; set; }
+    }
+}
